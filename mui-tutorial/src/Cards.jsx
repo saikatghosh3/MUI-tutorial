9c9bcfc18a5f6e3bba8b66fe803877f68eca72d5
Saikat Ghosh <saikat.blast20@gmail.com>
2024-12-26 21:25:45 +0600

    cards

--- mui-tutorial/src/Cards.jsx
+++ mui-tutorial/src/Cards.jsx
@@ -1,35 +1,67 @@
-import React from 'react'
-import {Card, CardContent, CardMedia, CardActions, Button, Typography} from '@mui/material'
+import React, { useState } from 'react';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from '@mui/material';
 
 const Cards = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <Card sx={{ maxWidth: "345px"}}>
+      <Card sx={{ maxWidth: 345, margin: 'auto' }}>
         <CardMedia
-             component={'img'}
-             height={"154"}
-             image='https://unsplash.com/photos/a-person-sitting-on-the-floor-0_9dFXVOZ4k'
-             alt='test image' />
-             <CardContent>
-                <Typography gutterBottom variant='h5' component="div">
-                    Web design
-                </Typography>
-                <Typography variant='body2'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, rem.
-                
-                
-                </Typography>
+          component="img"
+          height="154"
+          image="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=400"
+          alt="Test image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Web Design
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, rem.
+          </Typography>
+        </CardContent>
 
-
-
-             </CardContent>
-
-             <CardActions>
-                <Button size='small' color='primary'>Share</Button>
-             </CardActions>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary" onClick={() => setOpen(true)}>
+            Delete
+          </Button>
+        </CardActions>
       </Card>
-    </div>
-  )
-}
 
-export default Cards
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Are you sure?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this product?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={() => alert('Product deleted')} color="error">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Cards;
